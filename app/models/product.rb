5733cb2b8cdb1d45cc7f4ab8994ee0c22e6238fa
nikkiHelloThp <nicolas.stanojevic.lpo@gmail.com>
2020-06-24 14:56:15 +0200

    Validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,10 @@
 class Product < ApplicationRecord
+	validates :title, :description, :image_url, presence: true
+	validates :price, numericality: {greater_than_or_equal_to: 0.01}
+	validates :title, uniqueness: true
+	# allow_blank only allows the blank error to display if the field is blank
+	validates :image_url, allow_blank: true, format: {
+		with:    %r{\.(gif|jpg|jpeg|png)\Z}i,
+		message: 'must be a URL for GIF, JPG, JPEG or PNG image.'
+	}
 end
