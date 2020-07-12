5b5a39aa891e8dc5db32fb91369f9d7c6025e7e0
nikkiHelloThp <nicolas.stanojevic.lpo@gmail.com>
2020-07-12 14:04:49 +0200

    I18n

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,10 +10,6 @@ class ApplicationController < ActionController::Base
 			end
 		end
 
-		def default_url_options
-			{ locale: I18n.locale }
-		end
-
 		def set_i18n_locale_from_params
 			if params[:locale]
 				if I18n.available_locales.map(&:to_s).include?(params[:locale])
